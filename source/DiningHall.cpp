c9d500de46f0da0b943f9c4eb0eb783db79c594e
yousof-jafari-2004 <jafariyousef24@gmail.com>
2025-04-23 15:14:11 -0700

    created ReservationClass and DiningHall class

---
+++ source/DiningHall.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+class DiningHall {
+    private:
+        int hallId;
+        string name, address;
+        int capacity;
+
+
+};
